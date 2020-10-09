c82cf4bcffb64eeb87e895c89b992ed3757a8afb
guopeng li <guopeng.li@technicolor.com>
2020-10-09 09:10:33 +0200

    custom button with linearlayout ripple effet

--- app/src/main/java/com/lig/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/lig/a7minuteworkout/MainActivity.kt
@@ -2,10 +2,16 @@ package com.lig.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        llStart.setOnClickListener {
+            Toast.makeText(this, "Here we will start the exercise", Toast.LENGTH_SHORT).show()
+        }
     }
 }
